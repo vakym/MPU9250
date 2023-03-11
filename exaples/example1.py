de63eaf8f52988aa1e3e029949b1af2d2929e093
avakym <avakym>
2023-03-11 17:54:29 +0300

    fix 3

--- exaples/example1.py
+++ exaples/example1.py
@@ -7,8 +7,8 @@ from MPU9250.MPU9250 import MPU9250
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-b", "--bus", help="I2C bus number")
-    parser.add_argument("-a", "--address", help="Device address on I2C bus")
+    parser.add_argument("-b", "--bus", help="I2C bus number", type=int)
+    parser.add_argument("-a", "--address", help="Device address on I2C bus", type=int)
     args = parser.parse_args()
     sensor = MPU9250(args.bus, args.address)
     sensor.init()
