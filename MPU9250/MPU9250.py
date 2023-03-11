218079ccdbcaa37ae37b733e25cda18ce322455d
avakym <avakym>
2023-03-11 17:29:46 +0300

    add more func

--- MPU9250/MPU9250.py
+++ MPU9250/MPU9250.py
@@ -1,11 +1,12 @@
 from MPU9250.Registers import ADDRESS, REGISTER
-import smbus
+from smbus2 import SMBus
 import time
 
 
 class MPU9250:
     _bus = None
-    _chip_id = 0x71
+    _chipId = 0x71
+    _gyroOffset = (0,0,0)
 
     def __init__(self, bus_number):
         self._bus_number = bus_number
@@ -13,9 +14,10 @@ class MPU9250:
     def init(self):
         if self._bus:
             self.close()
-        self._bus = smbus.SMBus(self._bus_number)
-        real_chip_id = self._bus.read_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.WHO_AM_I)
-        if self._chip_id != real_chip_id:
+        self._bus = SMBus(self._bus_number)
+        self._bus.open()
+        realChipId = self._bus.read_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.WHO_AM_I)
+        if self._chipId != realChipId:
             raise Exception("Not correct chip id")
         self._bus.write_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.PWR_MGMT_1, 0x00)
         self._bus.write_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.SMPLRT_DIV, 0x07)
@@ -23,6 +25,7 @@ class MPU9250:
         self._bus.write_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.GYRO_CONFIG, 0x10)
         self._bus.write_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.GYRO_CONFIG, 0x10)
         time.sleep(1) #let the sensor be alone with itself
+        self._gyroOffset = self.__initGyroOffset()
 
     def readAccel(self):
         x = self._bus.read_word_data(ADDRESS.ACCEL_ADDRESS, REGISTER.ACCEL_XOUT_H)
@@ -32,7 +35,28 @@ class MPU9250:
     
     def getChipId(self):
         return self._bus.read_byte_data(ADDRESS.GYRO_ADDRESS, REGISTER.WHO_AM_I)
+    
+    def readGyro(self):
+        x = self._bus.read_word_data(ADDRESS.GYRO_ADDRESS, REGISTER.GYRO_XOUT_H)
+        y = self._bus.read_word_data(ADDRESS.GYRO_ADDRESS, REGISTER.GYRO_YOUT_H)
+        z = self._bus.read_word_data(ADDRESS.GYRO_ADDRESS, REGISTER.GYRO_ZOUT_H)
+        return (x,y,z)
+    
+    #TODO digit filter
 
     def close(self):
         if self._bus:
             self._bus.close()
+
+    def __initGyroOffset(self):
+        count = 32
+        x = 0
+        y = 0
+        z = 0
+        for number in range(count):
+            #TODO digit filter
+            data = self.readGyro()
+            x += data[0]
+            y += data[1]
+            z += data[2]
+        return (x / count, y / count, z / count)
---
+++ exaples/example2.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from MPU9250.MPU9250 import MPU9250
+
+if __name__ == "__main__":
+    sensor = MPU9250(1)
+    sensor.init()
+    print(sensor.getChipId())
+    while True:
+        print(" Gyro: ")
+        print(sensor.readGyro())
+        print(" Accel: ")
+        print(sensor.readAccel())
